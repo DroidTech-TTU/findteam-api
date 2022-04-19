42e51a6c5817a13bc3f7be2fa951d6953db1ff57
Rory Eckel <16675082+roryeckel@users.noreply.github.com>
2022-04-18 21:13:47 -0500

    Test db (failing)

--- test_app/test_db.py
+++ test_app/test_db.py
@@ -1,3 +1,17 @@
-from . import client
+"""
+Testing database creation and fetching
+"""
 
-# TODO
+import pytest
+from app.db import get_db, init_models
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+@pytest.mark.anyio
+async def test_get_db():
+    async_session = await anext(get_db())
+    assert isinstance(async_session, AsyncSession)
+
+@pytest.mark.anyio
+async def test_init_models():
+    await init_models()
