fe0928dd11a150174e3a60f0d7fe8134874368c1
Rory Eckel <16675082+roryeckel@users.noreply.github.com>
2022-04-23 13:03:58 -0500

    Initial testing

--- test_app/__init__.py
+++ test_app/__init__.py
@@ -5,12 +5,8 @@ FindTeam FastAPI test package
 __author__ = 'DroidTech'
 
 from logging import DEBUG
-from pathlib import Path
-from tempfile import gettempdir
 
 from app.config import settings
 
 settings.db_url = 'mysql+aiomysql://root:root@127.0.0.1/findteam'
-#settings.email_password = '123'
 settings.logging_level = DEBUG
-settings.picture_storage = Path(gettempdir())
--- test_app/test_main.py
+++ test_app/test_main.py
@@ -5,15 +5,14 @@ FindTeam FastAPI test app
 from app.main import app
 from fastapi.testclient import TestClient
 from pytest import fixture
+from app.models import MembershipType, Status
+
 
 @fixture
 def client():
     with TestClient(app) as c:
         yield c
 
-def test_index(client):
-    response = client.get('/')
-    print(response)
 
 def test_register(client):
     response = client.post('/register', json={
@@ -23,5 +22,40 @@ def test_register(client):
         'email': 'testing@user.com',
         'password': 'hunter2'
     })
-    #assert response.status_code == 200
-    # TODO
+    assert response.status_code == 200
+
+
+def test_login(client):
+    response = client.post('/login', json={
+        'username': 'testing@user.com',
+        'password': 'hunter2',
+        'grant_type': 'password'
+    })
+    assert response.status_code == 200
+
+
+def test_create_project(client):
+    response = client.post('/login', json={
+        'username': 'testing@user.com',
+        'password': 'hunter2',
+        'grant_type': 'password'
+    })
+    assert response.status_code == 200
+    response = client.post('/create', json={
+        'title': 'A Very Cool Project',
+        'status': Status.AWAITING_TEAM,
+        'description': 'This is a test.',
+        'members': [],
+        'tags': [
+            {
+                'text': 'Lubbock',
+                'category': 'Location',
+                'is_user_requirement': True
+            }
+        ]
+    })
+    assert response.status_code == 200
+
+
+def test_search(client):
+    raise NotImplementedError
